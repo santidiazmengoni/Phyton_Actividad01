cd4d7bf7e81ef1c49619c9a219d6066fa882b67a
Santiago Diaz Mengoni <santiagodiazmengoni@gmail.com>
2026-03-24 20:28:16 -0300

    resolucion ejercicio 3

---
+++ ActividadesIntroductorias/punto3.py
@@ -0,0 +1,9 @@
+# SOLICITO NUMERO
+numero = int(input("Ingrese un número: "))
+
+# MOSTRAR TABLA DE MULTIPLICACION
+for i in range(1,11):
+    result = numero * i
+    print(f"{numero} x {i} = {result}")
+
+    
