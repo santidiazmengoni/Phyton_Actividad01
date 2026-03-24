64cee639e27fecc73693ab4b02e718fb8a59bde2
Santiago Diaz Mengoni <santiagodiazmengoni@gmail.com>
2026-03-24 20:10:32 -0300

    resolucion ejercicio 1

---
+++ ActividadesIntroductorias/punto1.py
@@ -0,0 +1,5 @@
+año_nacimiento = int(input("Ingrese su año de nacimiento "))
+
+print(f"Cumplira 18 años en el {año_nacimiento + 18}")
+print(f"Cumplira 21 años en el {año_nacimiento + 21}")
+print(f"Cumplira 100 años en el {año_nacimiento + 100}")
